5c8433f276f1c87a44b119a3748fb0bc51b09717
Sana Siddiqui <sanafaizsiddiqui@me.com>
2021-05-28 02:28:48 +0400

    msg

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/noteful',
+  DATABASE_URL: 'postgres://ooilipemvcsune:171a31df4363442e38ee80d18ad7953dddfe2d2c00a0ec3e48e84f00bc15ed5d@ec2-54-243-92-68.compute-1.amazonaws.com:5432/dbgfb9c2eb33na&ssl=true',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/noteful'
 }
--- src/server.js
+++ src/server.js
@@ -3,10 +3,10 @@ const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
 const pg = require ('pg')
 
-pg.defaults.ssl = process.env.NODE_ENV === "production"
+// pg.defaults.ssl = process.env.NODE_ENV === "production"
 const db = knex({
   client: 'pg',
-  connection: DATABASE_URL,
+  connection: "postgres://ooilipemvcsune:171a31df4363442e38ee80d18ad7953dddfe2d2c00a0ec3e48e84f00bc15ed5d@ec2-54-243-92-68.compute-1.amazonaws.com:5432/dbgfb9c2eb33na&ssl=true",
 })
 
 app.set('db', db)
